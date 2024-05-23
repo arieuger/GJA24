582e6f30c9f86674cb1ff37e9663ad95f176cfd8
A. Roig <antiaroig@gmail.com>
2024-05-23 18:04:48 +0200

    Fadeout menú

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,12 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+
+    [SerializeField] private GameObject levelChanger;
+
     public void OnPlayButton()
     {
+        levelChanger.SetActive(true);
+        levelChanger.GetComponent<Animator>().Play("FadeOut");
+
+        StartCoroutine(waitTilNextScene());
+    }
+
+    private IEnumerator waitTilNextScene()
+    {
+        float remainingTime = 1.5f;
+        while (remainingTime > 0f)
+        {
+            remainingTime -= Time.deltaTime;
+            yield return null;            
+        }
         SceneManager.LoadScene(1);
     }
 }
