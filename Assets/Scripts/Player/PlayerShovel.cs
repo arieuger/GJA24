e8e8cb2828e7954c57f26e51dd2172c23e5ccb8d
A. Roig <antiaroig@gmail.com>
2024-05-30 15:20:30 +0200

    Partículas fume

--- Assets/Scripts/Player/PlayerShovel.cs
+++ Assets/Scripts/Player/PlayerShovel.cs
@@ -1,17 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerShovel : MonoBehaviour
 {
     
+    [SerializeField] private GameObject smokeParticlesPrefab;
     public bool IsDestructing { get; private set; }
-    
+    private ParticleSystem _smokeParticles;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag.Equals("DestructionZone"))
         {
-            if (!IsDestructing && !PlayerMovement.Instance.isEndingGame) other.gameObject.GetComponentInParent<Building>().StartDestruction();
+            if (!IsDestructing && !PlayerMovement.Instance.isEndingGame)
+            {
+                other.gameObject.GetComponentInParent<Building>().StartDestruction();
+                _smokeParticles = Instantiate(smokeParticlesPrefab, other.ClosestPoint(transform.position), Quaternion.identity).GetComponent<ParticleSystem>();
+                _smokeParticles.Play();
+            }
             IsDestructing = true;
         }
         
@@ -23,6 +32,7 @@ public class PlayerShovel : MonoBehaviour
         {
             if (IsDestructing)
             {
+                _smokeParticles.Stop();
                 var building = other.gameObject.GetComponentInParent<Building>();
                 if (building != null)
                 {
